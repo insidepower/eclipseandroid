c1ebba9a943c061061269150d2914000b1f0f506
insidepower <insidepower@insidepower-laptop.(none)>
2010-12-31 23:40:51 +0800

    added onPause; check if enter or down key is pressed

--- HelloAndroid/src/com/example/helloandroid/HelloAndroid.java
+++ HelloAndroid/src/com/example/helloandroid/HelloAndroid.java
@@ -13,20 +13,20 @@ public class HelloAndroid extends Activity {
 	boolean m_bSplashActive = true;
 	private static final String TAG = "Splash";
 
-    /** Called when the activity is first created. */
-    @Override
-    public void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-        setContentView(R.layout.main);
-        //TextView tv = new TextView(this);
-        //tv.setText("My Hello Android");
-        //setContentView(tv);
+	/** Called when the activity is first created. */
+	@Override
+	public void onCreate(Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		setContentView(R.layout.main);
+		//TextView tv = new TextView(this);
+		//tv.setText("My Hello Android");
+		//setContentView(tv);
 
 		/// very simple timer thread
 		Thread splashTimer = new Thread(){
 			public void run(){
 				try{
-				/// wait loop
+					/// wait loop
 					long ms = 0;
 					while(m_bSplashActive && ms < m_dwSplashTime){
 						sleep(100);
@@ -36,7 +36,7 @@ public class HelloAndroid extends Activity {
 					}
 					/// Advance to next screen
 					startActivity (new Intent (
-								"com.example.helloandroid.CLEARSPLASH"));
+					"com.example.helloandroid.CLEARSPLASH"));
 				}catch(Exception e){
 					Log.e(TAG, e.toString());
 				}finally{
@@ -45,15 +45,50 @@ public class HelloAndroid extends Activity {
 			}
 		};
 		splashTimer.start();
-    }
+	}
+
+	protected void onStart()
+	{
+		super.onPause();
+		Log.d(TAG, "@@ onStart @@ ");
+	}
+
+	protected void onPause()
+	{
+		super.onPause();
+		m_bPaused = true;
+		Log.d(TAG, "@@ onPause @@ ");
+	}
+
+	protected void onResume()
+	{
+		super.onResume();
+		m_bPaused = false;
+		Log.d(TAG, "@@ onResume @@ ");
+	}
+
+	protected void onDestroy()
+	{
+		super.onDestroy();
+		Log.d(TAG, "@@ onDestroy @@ ");
+	}
 
 	@Override
 	public boolean onKeyDown(int keyCode, KeyEvent event)
 	{
-		//if we get any key, clear the splash screen
 		super.onKeyDown(keyCode, event);
-		m_bSplashActive = false;
 		Log.d(TAG, "onKeyDown is called");
-		return true;
+		//if we get any key, clear the splash screen
+		if (keyCode == KeyEvent.KEYCODE_DPAD_CENTER ||
+				keyCode == KeyEvent.KEYCODE_ENTER) {
+			m_bSplashActive = false;
+			Log.d(TAG, "Enter or Center key is pressed");
+			return true;
+		}else if(keyCode == KeyEvent.KEYCODE_BACK) {
+			Log.d(TAG, "Back key is pressed");
+			return true;
+		}else {
+			return false;
+		}
 	}
 }
