3c362c3cde1235438649208e96c656425bbd38ed
insidepower <insidepower@insidepower-laptop.(none)>
2011-01-01 09:54:55 +0800

    intent setClassName is working

--- HelloAndroid/src/com/example/helloandroid/MainView.java
+++ HelloAndroid/src/com/example/helloandroid/MainView.java
@@ -3,11 +3,13 @@ package com.example.helloandroid;
 import android.app.Activity;
 import android.content.Intent;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 
 public class MainView extends Activity {
 	Button btn;
+	private static final String TAG = "MainView";
     /** Called when the activity is first created. */
     @Override
     public void onCreate(Bundle savedInstanceState) {
@@ -24,5 +26,21 @@ public class MainView extends Activity {
 				startActivity(myIntent);
      		}
         });
+
+		btn = (Button) findViewById(R.id.invokeExtInt2);
+		btn.setOnClickListener(new View.OnClickListener() {
+			public void onClick(View view) {
+				try {
+					Intent i = new Intent();
+					i.setClassName("com.example.viewOnClickShowTime",
+						"com.example.viewOnClickShowTime.intentReceiver");
+					i.putExtra( "i1", new Integer( 5 ) );
+					i.putExtra( "i2", new Integer( 6 ) );
+					startActivity(i);
+				}catch (Exception e) {
+					Log.d(TAG, e.toString());
+				}
+			}
+		});
     }
 }
