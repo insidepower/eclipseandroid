ca22cc62121a59c8186dacce490d3e62832cd841
insidepower <insidepower@insidepower-laptop.(none)>
2011-01-01 18:17:51 +0800

    resolved intent is null problem

--- HelloAndroid/src/com/example/helloandroid/MainView.java
+++ HelloAndroid/src/com/example/helloandroid/MainView.java
@@ -6,10 +6,14 @@ import android.os.Bundle;
 import android.util.Log;
 import android.view.View;
 import android.widget.Button;
+import android.widget.TextView;
 
 public class MainView extends Activity {
 	Button btn;
+	Button btn2;
+	Button btn3;
 	private static final String TAG = "MainView";
+	private static final int REQ_INTENT_CODE = 10;
     /** Called when the activity is first created. */
     @Override
     public void onCreate(Bundle savedInstanceState) {
@@ -27,8 +31,8 @@ public class MainView extends Activity {
      		}
         });
 
-		btn = (Button) findViewById(R.id.invokeExtInt2);
-		btn.setOnClickListener(new View.OnClickListener() {
+		btn2 = (Button) findViewById(R.id.invokeExtInt2);
+		btn2.setOnClickListener(new View.OnClickListener() {
 			public void onClick(View view) {
 				try {
 					Intent i = new Intent();
@@ -42,5 +46,48 @@ public class MainView extends Activity {
 				}
 			}
 		});
+
+		btn3 = (Button) findViewById(R.id.invokeExtInt3);
+		btn3.setOnClickListener(new View.OnClickListener() {
+			public void onClick(View view) {
+				try {
+					/// wait for result which invoke onActivityResult()
+					Intent i = new Intent(
+						"com.example.helloandroid.fromSomeone");
+					i.putExtra( "i1", new Integer( 7 ) );
+					i.putExtra( "i2", new Integer( 8 ) );
+					startActivityForResult(i, REQ_INTENT_CODE);
+				}catch (Exception e) {
+					Log.d(TAG, e.toString());
+				}
+			}
+		});
     }
+
+	//protected void onActivityResult(int requestCode, int resultCode, 
+	@Override
+	protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+		super.onActivityResult(requestCode, resultCode, data);
+			Log.d(TAG,"@@ onActivityResult @@, requestCode="+requestCode
+					+"; resultCode="+resultCode);
+			switch(requestCode) {
+				case REQ_INTENT_CODE:
+					if (data != null){
+						Bundle extras = data.getExtras();
+						if (extras != null){
+							Integer r = data.getExtras().getInt("r");
+							Log.d(TAG,"@@ onActivityResult - REQ_INTENT_CODE @@ r="+r.intValue());
+							if (r != null ){
+								TextView t = (TextView)findViewById(R.id.result);
+								t.setText(r.toString());
+							}
+						}else{
+							Log.d(TAG,"@@ extras is null @@");
+						}
+					}else{
+							Log.d(TAG,"@@ intent is null @@");
+					}
+				break;
+		}
+	}
 }
--- ViewOnClickShowTime/src/com/example/viewOnClickShowTime/intentReceiver.java
+++ ViewOnClickShowTime/src/com/example/viewOnClickShowTime/intentReceiver.java
@@ -4,17 +4,20 @@ import android.app.Activity;
 import android.content.Intent;
 import android.os.Bundle;
 import android.util.Log;
+import android.view.KeyEvent;
 
 /* relative layout */ /* table layout */ /* scroll layout */
 public class intentReceiver extends Activity{
 	private static final String TAG = "IntentReceiver";
+	Intent myIntent = null;
+	Intent newIntent = null;
 
 	@Override
 	public void onCreate(Bundle state){
 		super.onCreate(state);
 		//setContentView(R.layout.myrelativelayout);
 		//setContentView(R.layout.mytablelayout);
-		Intent myIntent = getIntent();
+		myIntent = getIntent();
 		Bundle extras = null;
 		if ( myIntent != null )
 			extras = myIntent.getExtras();
@@ -24,10 +27,41 @@ public class intentReceiver extends Activity{
 			if (i1 != null && i2 != null){
 				Log.d(TAG,i1.toString()+i2.toString());
 				int res = i1.intValue()+i2.intValue();
-				extras.putInt("r", new Integer(res));
-				//setResult(RESULT_OK, null, extras);
+				myIntent.putExtra("r", new Integer(res));
+				//setResult(RESULT_OK,myIntent);
+				//finish();
 			}
 		}
 		setContentView(R.layout.myscrolllayout);
+	}
+
+	@Override
+	public boolean onKeyDown(int keyCode, KeyEvent event)
+	{
+		super.onKeyDown(keyCode, event);
+		Log.d(TAG, "## onKeyDown ##");
+		if (keyCode != KeyEvent.KEYCODE_BACK){
+			if (myIntent != null){
+				Log.d(TAG, "## myIntent is valid ##");
+				setResult(Activity.RESULT_OK,myIntent);
+			}
+			finish();
+			return true;
+		}else{
+			return false;
+		}
+	}
+
+	@Override
+	public void onBackPressed ()
+	{
+		//super.onBackPressed();
+		Log.d(TAG, "## onBackPressed ##");
+		if (myIntent != null){
+			Log.d(TAG, "## myIntent2 is valid ##");
+			setResult(Activity.RESULT_OK,myIntent);
+			//setResult(Activity.RESULT_OK,myIntent);
+		}
+		finish();
 	}
 }
