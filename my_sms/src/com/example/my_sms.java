364d2f2cd4051bed9b77f75d488da86c57bb8763
insidepower <insidepower@insidepower-laptop.(none)>
2011-01-02 01:56:38 +0800

    able to decode sms

--- my_sms/src/com/example/my_sms.java
+++ my_sms/src/com/example/my_sms.java
@@ -3,16 +3,40 @@ package com.example;
 import android.content.BroadcastReceiver;
 import android.content.Context;
 import android.content.Intent;
+import android.os.Bundle;
+import android.telephony.SmsMessage;
 import android.util.Log;
 
 public class my_sms extends BroadcastReceiver {
 	private static final String TAG = "my_sms";
+	private static final String SMS_RECEIVED = "android.provider.Telephony.SMS_RECEIVED";
 
 	@Override
 	public void onReceive(Context context, Intent intent) {
 		Log.d(TAG, "onReceive");
-		Intent i = new Intent();
-		i.setAction("com.example.second");
-		context.sendBroadcast(i);
+		Intent my_intent = new Intent();
+		my_intent.setAction("com.example.second");
+		context.sendBroadcast(my_intent);
+
+		/// reading the sms
+		Log.d(TAG, "Intent received: " + intent.getAction());
+		if (intent.getAction().equals(SMS_RECEIVED)) {
+			Log.d(TAG, "SMS_RECEIVED");
+			Bundle bundle = intent.getExtras();
+			if (bundle != null) {
+				Log.d(TAG, "bundle not null");
+				Object[] pdus = (Object[])bundle.get("pdus");
+				final SmsMessage[] messages = new SmsMessage[pdus.length];
+				for (int i = 0; i < pdus.length; i++) {
+					messages[i] = SmsMessage.createFromPdu((byte[])pdus[i]);
+				}
+				if (messages.length > -1) {
+					Log.i(TAG, "Message received: " + messages[0].getMessageBody());
+					//NetComm.SendMessage("me", messages[0].getOriginatingAddress(), messages[0].getMessageBody());
+				}
+			}
+		}else{
+			Log.d(TAG, " @@ not sms @@");
+		}
 	}
 }
