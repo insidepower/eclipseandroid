36a70f462ff33864b884178075198b18972d15c5
insidepower <insidepower@insidepower-laptop.(none)>
2011-01-04 22:49:29 +0800

    added Preference from resources

--- sudoku/src/com/example/sudoku.java
+++ sudoku/src/com/example/sudoku.java
@@ -3,8 +3,10 @@ package com.example;
 import android.app.Activity;
 import android.content.Intent;
 import android.os.Bundle;
+import android.provider.Settings;
 import android.view.Menu;
 import android.view.MenuInflater;
+import android.view.MenuItem;
 import android.view.View;
 import android.view.View.OnClickListener;
 
@@ -39,10 +41,22 @@ public class sudoku extends Activity implements OnClickListener{
 	
 	@Override
 	public boolean onCreateOptionsMenu(Menu menu) {
-	super.onCreateOptionsMenu(menu);
-	MenuInflater inflater = getMenuInflater();
-	inflater.inflate(R.menu.menu, menu);
-	return true;
+		super.onCreateOptionsMenu(menu);
+		MenuInflater inflater = getMenuInflater();
+		inflater.inflate(R.menu.menu, menu);
+		return true;
 	}
+
+	@Override
+	public boolean onOptionsItemSelected(MenuItem item) {
+		switch (item.getItemId()) {
+		case R.id.settings:
+			startActivity(new Intent(this, com.example.Settings.class));
+			return true;
+			// More items go here (if any) ...
+		}
+		return false;
+	}
+
 
 }
