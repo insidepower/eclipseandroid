466d7757ba0ba16a2fd5972fd0ac3d24fdce2006
insidepower <insidepower@insidepower-laptop.(none)>
2011-03-09 22:40:57 +0800

    ealier spawn services to set to corect date

--- earlier/src/kn/app/earlier/setActualTime.java
+++ earlier/src/kn/app/earlier/setActualTime.java
@@ -3,63 +3,54 @@ package kn.app.earlier;
 import java.io.IOException;
 import java.util.Calendar;
 
-import android.app.Activity;
+import android.app.Service;
 import android.content.Context;
 import android.content.Intent;
-import android.os.Bundle;
 import android.os.Handler;
+import android.os.IBinder;
 import android.util.Log;
-import android.widget.TextView;
 import android.widget.Toast;
 
-public class earlierdate extends Activity {
-	private TextView mDateDisplay;
+public class setActualTime extends Service{
 	private int mYear;
 	private int mMonth;
 	private int mDay;
 	private int mHour, mMin, mSec;
-
+	private static final String TAG = "EarlierService";
 	private Handler mHandler = new Handler();
-	private static final String TAG = "Earlier";
 
-	/** Called when the activity is first created. */
 	@Override
-	public void onCreate(Bundle savedInstanceState) {
-		super.onCreate(savedInstanceState);
-		setContentView(R.layout.main);
-		mDateDisplay = (TextView) findViewById(R.id.dateDisplay);
-
-		getDate();
-		mYear -= 1;  // set to earlier one year
-		//Log.i(TAG, "yyyy-mm-dd = "+mYear+"-"+mMonth+"-"+mDay);
-		//Log.i(TAG, "hh:mm:ss = "+mHour+":"+mMin+":"+mSec);
-		//Log.i(TAG, mDate.toString());
-		setDate(strBuilder(mYear, mMonth, mDay, mHour, mMin, mSec));
-		mDateDisplay.setText(
-				new StringBuilder()
-				.append(mMonth).append("-")
-				.append(mDay).append("-")
-				.append(mYear).append(" "));
-	
-		mHandler.postDelayed(mUpdateTimeTask, 500);
+	public IBinder onBind(Intent arg0) {
+		// TODO Auto-generated method stub
+		return null;
 	}
 
 	@Override
-	public void onStart() {
-		super.onStart();
-		stopService(new Intent(this, setActualTime.class));
-		startService(new Intent(this, setActualTime.class));
-		//Toast.makeText(this, TAG+" onStart()", Toast.LENGTH_SHORT).show();
+	public void onCreate() {
+		//Toast.makeText(this, "inside services", Toast.LENGTH_SHORT).show();
+		//Log.d(TAG, "onCreate");
+		//getDate();
+		//mYear+=1; // set back to original year
+		//setDate(strBuilder(mYear, mMonth, mDay, mHour, mMin, mSec));
+		mHandler.postDelayed(mUpdateTimeTask, 20000);
 	}
 
-	/*
+	private Runnable mUpdateTimeTask = new Runnable() {
+		public void run() {
+			getDate();
+			mYear+=1; // set back to original year
+			setDate(strBuilder(mYear, mMonth, mDay, mHour, mMin, mSec));
+			onDestroy();
+			//finish();
+		}
+	};
+
 	@Override
-	public void onResume() {
-		super.onResume();
-		Toast.makeText(this, TAG+" onResume()", Toast.LENGTH_SHORT).show();
+	public void onDestroy() {
+		Toast.makeText(this, TAG+" onDestroy()", Toast.LENGTH_SHORT).show();
+		//Log.d(TAG, "onDestroy()");
 	}
-	*/
-	
+
 	private void getDate() {
 		final Calendar c = Calendar.getInstance();
 		mYear = c.get(Calendar.YEAR);
@@ -69,7 +60,7 @@ public class earlierdate extends Activity {
 		mMin = c.get(Calendar.MINUTE);
 		mSec = c.get(Calendar.SECOND);
 	}
-	
+
 	private String strBuilder(int year, int mon, int day, 
 			int hr, int min, int sec) {
 		StringBuilder mDate = new StringBuilder();
@@ -84,7 +75,7 @@ public class earlierdate extends Activity {
 		mDate.append(mMin);
 		if(mSec<10) mDate.append("0");
 		mDate.append(mSec);
-		
+
 		return mDate.toString();
 	}
 
@@ -112,12 +103,4 @@ public class earlierdate extends Activity {
 
 	}
 
-	private Runnable mUpdateTimeTask = new Runnable() {
-		public void run() {
-			//getDate();
-			//mYear+=1; // set back to original year
-			//setDate(strBuilder(mYear, mMonth, mDay, mHour, mMin, mSec));
-			finish();
-		}
-	};
 }
