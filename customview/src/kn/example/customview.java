0382a9133c86b6b9ac3b6b38848a7a5d8a899b97
insidepower <insidepower@insidepower-laptop.(none)>
2011-03-13 21:35:54 +0800

    todo item is working

--- customview/src/kn/example/customview.java
+++ customview/src/kn/example/customview.java
@@ -4,23 +4,36 @@ import java.util.ArrayList;
 
 import android.app.Activity;
 import android.os.Bundle;
+import android.view.View;
 import android.widget.ArrayAdapter;
+import android.widget.Button;
 import android.widget.EditText;
 import android.widget.ListView;
 
 public class customview extends Activity {
+	private ArrayList<String> todoItems;
+	EditText myEditText;
+	ArrayAdapter<String> aa;
     /** Called when the activity is first created. */
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.main);
         
-        final EditText myEditText = (EditText)findViewById(R.id.myEditText);
+        myEditText = (EditText)findViewById(R.id.myEditText);
         ListView myListView = (ListView) findViewById(R.id.customListView);
+        Button btnAddItem = (Button) findViewById(R.id.btnAddItem);
         
-        final ArrayList<String> todoItems = new ArrayList<String> ();
+        todoItems = new ArrayList<String> ();
         int resID = R.layout.customlistlayout;
-        final ArrayAdapter<String> aa = new ArrayAdapter<String>(this, resID, todoItems);
+        aa = new ArrayAdapter<String>(this, resID, todoItems);
         myListView.setAdapter(aa);
     }
+    
+    public void addNewItem(View view) {
+    	todoItems.add(0,myEditText.getText().toString());
+    	myEditText.setText("");
+        aa.notifyDataSetChanged();
+    }
+
 }
