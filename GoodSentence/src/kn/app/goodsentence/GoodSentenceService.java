5e206c58699d75d15074b616278a370e887b274d
insidepower <insidepower@insidepowerpc>
2013-01-25 10:26:56 +0800

    multiple widgets works, added all GoodSaying Files

--- GoodSentence/src/kn/app/goodsentence/GoodSentenceService.java
+++ GoodSentence/src/kn/app/goodsentence/GoodSentenceService.java
@@ -5,6 +5,7 @@ import android.app.Service;
 import android.appwidget.AppWidgetManager;
 import android.content.Context;
 import android.content.Intent;
+import android.net.Uri;
 import android.os.IBinder;
 import android.util.Log;
 import android.widget.RemoteViews;
@@ -19,6 +20,13 @@ public class GoodSentenceService extends Service {
         Intent intent = new Intent(context, GoodSentenceService.class);
         intent.setAction(command);
         intent.putExtra(AppWidgetManager.EXTRA_APPWIDGET_ID, appWidgetId);
+        /// must set, else all widget are treat as same by android
+        /// e.g. android will treat total instance as one
+        Uri data = Uri.withAppendedPath(
+                Uri.parse("String" + "://widget/id/")
+                ,String.valueOf(appWidgetId));
+        intent.setData(data);
+
         return (PendingIntent.getService(context, 0, intent, PendingIntent.FLAG_UPDATE_CURRENT));
     }
 
--- GoodSentence/src/kn/app/goodsentence/GoodSentenceWidget.java
+++ GoodSentence/src/kn/app/goodsentence/GoodSentenceWidget.java
@@ -4,8 +4,10 @@ import android.appwidget.AppWidgetManager;
 import android.appwidget.AppWidgetProvider;
 import android.content.Context;
 import android.content.Intent;
+import android.util.Log;
 
 public class GoodSentenceWidget extends AppWidgetProvider {
+    public static final String TAG = "GdSenteceWidget";
 
     public void onEnabled(Context context){
     }
@@ -15,10 +17,12 @@ public class GoodSentenceWidget extends AppWidgetProvider {
                     AppWidgetManager appWidgetMgr,
                     int[] appWidgetIds) {
         final int N = appWidgetIds.length;
+        Log.i(TAG, "appWidget len="+N);
 		// Perform this loop procedure for each App Widget that belongs to this provider
-		// as we can create a few widget, this will update all?
+		// as we can create a few widget, this will update all
 		for (int appWidgetId : appWidgetIds) {
 			//setAlarm(context, appWidgetId, UPDATE_RATE);
+            Log.i(TAG, "appWidgetId="+appWidgetId);
             Intent intent = new Intent(context, GoodSentenceService.class);
             intent.setAction(GoodSentenceService.UPDATE);
             intent.putExtra(AppWidgetManager.EXTRA_APPWIDGET_ID, appWidgetId);
