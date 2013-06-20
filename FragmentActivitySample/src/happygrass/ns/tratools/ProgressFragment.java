eeedfc50180c1e208cf10c676b9bf7dc97dac1ca
ken <mailingonly@gmail.com>
2013-06-21 00:31:43 +0800

    added fragment example

---
+++ FragmentActivitySample/src/happygrass/ns/tratools/ProgressFragment.java
@@ -0,0 +1,31 @@
+package happygrass.ns.tratools;
+
+import android.os.Bundle;
+import android.support.v4.app.Fragment;
+import android.util.Log;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.TextView;
+
+public class ProgressFragment extends Fragment {
+	@Override
+	public void onCreate(Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+	}
+
+	@Override
+	public void onActivityCreated(Bundle savedInstanceState) {
+		super.onActivityCreated(savedInstanceState);
+	}
+
+	@Override
+	public View onCreateView(LayoutInflater inflater, ViewGroup container,
+			Bundle savedInstanceState) {
+		View view = inflater.inflate(R.layout.data_fragment, container, false);
+		TextView textView = (TextView) view.findViewById(R.id.detailsText);
+		textView.setText("ProgressFragment");
+
+		return view;
+	}
+}
