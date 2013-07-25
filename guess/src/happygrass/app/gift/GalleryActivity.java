3b80246d1c536e2091fb9b7b0c4c6361003321e2
ken <mailingonly@gmail.com>
2013-07-25 23:35:23 +0800

    added bday and scroll text

--- guess/src/happygrass/app/gift/GalleryActivity.java
+++ guess/src/happygrass/app/gift/GalleryActivity.java
@@ -4,20 +4,65 @@ import happygrass.app.gift.R;
 import android.app.Activity;
 import android.media.MediaPlayer;
 import android.os.Bundle;
+import android.view.Window;
+import android.view.WindowManager;
+import android.view.animation.Animation;
+import android.widget.ImageView;
+import android.graphics.drawable.AnimationDrawable;
+import android.view.animation.AnimationUtils;
+import android.widget.TextView;
 
 public class GalleryActivity extends Activity {
+	MediaPlayer mp;
+
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
+
+		requestWindowFeature(Window.FEATURE_NO_TITLE);
+		getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN, 
+								WindowManager.LayoutParams.FLAG_FULLSCREEN);
+
 		setContentView(R.layout.gallery_act);
 
-		MediaPlayer mp=new MediaPlayer();
+		ImageView img_cake = (ImageView) findViewById(R.id.img_cake);
+		img_cake.setBackgroundResource(R.drawable.animate_cake);
+		AnimationDrawable img_cake_ani = (AnimationDrawable) img_cake.getBackground();
+		img_cake_ani.start();
+
+		Animation ani_text_up = AnimationUtils.loadAnimation(
+									this, R.anim.anim_text_scroll);
+		TextView tv = (TextView) findViewById(R.id.gallery_text);
+		tv.startAnimation(ani_text_up);
+
 		try {
-			mp.setDataSource("/sdcard/mp3/song.mp3");
+			mp = new MediaPlayer();
+			mp.setDataSource("/sdcard/music/song.mp3");
 			mp.prepare();
+			mp.setVolume(1.0f, 1.0f);
 			mp.start();
 		}catch(Exception e) {
 			e.printStackTrace();
+		}
+	}
+
+	@Override
+	protected void onPause () {
+		super.onPause();
+		if (mp != null ){
+			mp.stop();
+			mp.release();
+			mp = null;
+		}
+	}
+
+	@Override
+	protected void onDestroy () {
+		super.onDestroy();
+		if (mp != null ){
+			mp.stop();
+			mp.release();
+			mp = null;
 		}
 	}
 }
