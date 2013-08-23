33155af7c37775d158779d240d6a0d0061b4a9ab
Ken <insidepower2@gmail.com>
2013-08-23 12:29:55 +0800

    added dimen and shape

--- android-tutorial/Text.java
+++
@@ -1,9 +0,0 @@
-[ EditText ]
-
-EditText et_passwd = (EditText) findViewById(R.id.txt_password);
-String passwd = et_passwd.getText().toString();
-if (passwd.equals("03012007")) {
-[ ------------------------------------------------------------------ ]
-
-=== [ java:topic ][ String format ][ print ] ===
-et_passwd.setText(String.format("%.02f", 0.05));
