798d9276d50bb6db76fea159e9e015519edf2555
ken <mailingonly@gmail.com>
2013-11-15 10:40:06 +0800

    added back diskusage file

---
+++ reference/web/diskusage/src/com/google/android/diskusage/MountPoint.java
@@ -0,0 +1,269 @@
+/**
+ * DiskUsage - displays sdcard usage on android.
+ * Copyright (C) 2008-2011 Ivan Volosyuk
+ *
+ * This program is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU General Public License
+ * as published by the Free Software Foundation; either version 2
+ * of the License, or (at your option) any later version.
+
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+
+ * You should have received a copy of the GNU General Public License
+ * along with this program; if not, write to the Free Software
+ * Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301, USA.
+ */
+
+package com.google.android.diskusage;
+
+import java.io.BufferedReader;
+import java.io.File;
+import java.io.FileReader;
+import java.io.InputStreamReader;
+import java.util.ArrayList;
+import java.util.HashSet;
+import java.util.Map;
+import java.util.TreeMap;
+
+import android.app.Application;
+import android.content.Context;
+import android.os.Build;
+import android.os.Environment;
+import android.util.Log;
+
+import com.google.android.diskusage.entity.FileSystemEntry;
+import com.google.android.diskusage.entity.FileSystemEntry.ExcludeFilter;
+import com.google.android.diskusage.entity.FileSystemRoot;
+
+public class MountPoint {
+  FileSystemEntry.ExcludeFilter excludeFilter;
+  String title;
+  String root;
+  boolean hasApps2SD;
+  boolean rootRequired;
+  
+  MountPoint(String title, String root, ExcludeFilter excludeFilter,
+      boolean hasApps2SD, boolean rootRequired) {
+    this.title = title;
+    this.root = root;
+    this.excludeFilter = excludeFilter;
+    this.hasApps2SD = hasApps2SD;
+    this.rootRequired = rootRequired;
+  }
+  
+  private static MountPoint defaultStorage;
+  private static Map<String, MountPoint> mountPoints = new TreeMap<String, MountPoint>();
+  private static Map<String, MountPoint> rootedMountPoints = new TreeMap<String, MountPoint>();
+  private static boolean init = false;
+  private static MountPoint honeycombSdcard;
+  static int checksum = 0; 
+  
+  public static MountPoint getHoneycombSdcard(Context context) {
+    initMountPoints(context);
+    return honeycombSdcard;
+  }
+  
+  public static MountPoint getDefaultStorage(Context context) {
+    initMountPoints(context);
+    return defaultStorage;
+  }
+  
+  public static Map<String,MountPoint> getMountPoints(Context context) {
+    initMountPoints(context);
+    return mountPoints;
+  }
+  
+  public static Map<String,MountPoint> getRootedMountPoints(Context context) {
+    initMountPoints(context);
+    return rootedMountPoints;
+  }
+  
+  public static MountPoint getNormal(Context context, String rootPath) {
+    initMountPoints(context);
+    return mountPoints.get(rootPath);
+  }
+  
+  public static MountPoint forPath(Context context, String path) {
+	Log.d("diskusage", "Looking for mount point for path: " + path);
+    initMountPoints(context);
+    MountPoint match = null;
+    path = FileSystemRoot.withSlash(path);
+    for (MountPoint m : mountPoints.values()) {
+      if (path.contains(FileSystemRoot.withSlash(m.root))) {
+        if (match == null || match.root.length() < m.root.length()) {
+          Log.d("diskusage", "MATCH:" + m.root);
+          match = m;
+        }
+      }
+    }
+    for (MountPoint m : rootedMountPoints.values()) {
+      if (path.contains(FileSystemRoot.withSlash(m.root))) {
+        if (match == null || match.root.length() < m.root.length()) {
+          match = m;
+          Log.d("diskusage", "MATCH:" + m.root);
+        }
+      }
+    }
+
+    // FIXME: quick hack
+    if (match == null) {
+      Log.d("diskusage", "Use honeycomb hack for /data");
+      match = mountPoints.get("/data");
+    }
+    return match;
+  }
+  
+  public static MountPoint getRooted(Context context, String rootPath) {
+    initMountPoints(context);
+    return rootedMountPoints.get(rootPath);
+  }
+
+  public static boolean hasMultiple(Context context) {
+    initMountPoints(context);
+    return mountPoints.size() != 1;
+  }
+
+  public FileSystemEntry.ExcludeFilter getExcludeFilter() {
+    return excludeFilter;
+  }
+  
+  public String getRoot() {
+    return root;
+  }
+  
+  public static String storageCardPath() {
+    try {
+      return Environment.getExternalStorageDirectory().getCanonicalPath();
+    } catch (Exception e) {
+      return Environment.getExternalStorageDirectory().getAbsolutePath();
+    }
+  }
+  
+  private static void initMountPoints(Context context) {
+    if (init) return;
+    init = true;
+    String storagePath = storageCardPath();
+    ArrayList<MountPoint> mountPointsList = new ArrayList<MountPoint>();
+    HashSet<String> excludePoints = new HashSet<String>();
+    if (storagePath != null) {
+      defaultStorage = new MountPoint(
+              titleStorageCard(context), storagePath, null, false, false); 
+      mountPointsList.add(defaultStorage);
+      mountPoints.put(storagePath, defaultStorage);
+    }
+    
+    try {
+      // FIXME: debug
+      checksum = 0;
+      BufferedReader reader = new BufferedReader(new FileReader("/proc/mounts"));
+//      BufferedReader reader = new BufferedReader(new InputStreamReader(context.getResources().openRawResource(R.raw.mounts_honeycomb), "UTF-8"));
+      String line;
+      while ((line = reader.readLine()) != null) {
+        checksum += line.length();
+        String[] parts = line.split(" +");
+        if (parts.length < 3) continue;
+        String mountPoint = parts[1];
+        String fsType = parts[2];
+        if (!(fsType.equals("vfat") || fsType.equals("tntfs") || fsType.equals("exfat")
+            || fsType.equals("texfat"))
+            || mountPoint.startsWith("/mnt/asec")
+            || mountPoint.startsWith("/firmware")
+            || mountPoint.startsWith("/mnt/secure")
+            || mountPoint.startsWith("/data/mac")) {
+          excludePoints.add(mountPoint);
+          
+          // Default storage is not vfat, removing it (real honeycomb)
+          if (mountPoint.equals(storagePath)) {
+            mountPointsList.remove(defaultStorage);
+            mountPoints.remove(mountPoint);
+          }
+          if (/*rooted &&*/ !mountPoint.startsWith("/mnt/asec/")) {
+            mountPointsList.add(new MountPoint(mountPoint, mountPoint, null, false, true));
+          }
+        } else {
+          mountPointsList.add(new MountPoint(mountPoint, mountPoint, null, false, false));
+        }
+      }
+      
+      for (MountPoint mountPoint: mountPointsList) {
+        String prefix = mountPoint.root + "/";
+        boolean has_apps2sd = false;
+        ArrayList<String> excludes = new ArrayList<String>();
+        String mountPointName = new File(mountPoint.root).getName();
+        
+        for (MountPoint otherMountPoint : mountPointsList) {
+          if (otherMountPoint.root.startsWith(prefix)) {
+            excludes.add(mountPointName + "/" + otherMountPoint.root.substring(prefix.length()));
+          }
+        }
+        for (String otherMountPoint : excludePoints) {
+          if (otherMountPoint.equals(prefix + ".android_secure")) {
+            has_apps2sd = true;
+          }
+          if (otherMountPoint.startsWith(prefix)) {
+            excludes.add(mountPointName + "/" + otherMountPoint.substring(prefix.length()));
+          }
+        }
+        MountPoint newMountPoint = new MountPoint(
+            mountPoint.root, mountPoint.root, new ExcludeFilter(excludes),
+            has_apps2sd, mountPoint.rootRequired);
+        if (mountPoint.rootRequired) {
+          rootedMountPoints.put(mountPoint.root, newMountPoint);
+        } else {
+          mountPoints.put(mountPoint.root, newMountPoint);
+        }
+      }
+    } catch (Exception e) {
+      Log.e("diskusage", "Failed to get mount points", e);
+    }
+    final int sdkVersion = Integer.parseInt(Build.VERSION.SDK);
+    
+    if(sdkVersion >= Build.VERSION_CODES.HONEYCOMB
+        && mountPoints.get(storageCardPath()) == null) {
+      // No real /sdcard in honeycomb
+      honeycombSdcard = defaultStorage;
+      mountPoints.put("/data", new MountPoint(
+              titleStorageCard(context), "/data", null, false, false));
+    }
+
+    if (!mountPoints.isEmpty()) {
+      defaultStorage = mountPoints.values().iterator().next();
+      defaultStorage.title = titleStorageCard(context);
+    }
+  }
+  
+  private static String titleStorageCard(Context context) {
+    return context.getString(R.string.storage_card);
+  }
+  
+//  private static final String file = 
+//    "rootfs / rootfs ro,relatime 0 0\n" +
+//    "tmpfs /dev tmpfs rw,relatime,mode=755 0 0\n" +
+//    "devpts /dev/pts devpts rw,relatime,mode=600 0 0\n" +
+//    "proc /proc proc rw,relatime 0 0\n" +
+//    "sysfs /sys sysfs rw,relatime 0 0\n" +
+//    "none /acct cgroup rw,relatime,cpuacct 0 0\n" +
+//    "tmpfs /mnt/asec tmpfs rw,relatime,mode=755,gid=1000 0 0\n" +
+//    "none /dev/cpuctl cgroup rw,relatime,cpu 0 0\n" +
+//    "/dev/block/mtdblock3 /system yaffs2 ro,relatime 0 0\n" +
+//    "/dev/block/mtdblock5 /data yaffs2 rw,nosuid,nodev,relatime 0 0\n" +
+//    "/dev/block/mtdblock4 /cache yaffs2 rw,nosuid,nodev,relatime 0 0\n" +
+//    "/sys/kernel/debug /sys/kernel/debug debugfs rw,relatime 0 0\n" +
+//    "/dev/block/vold/179:1 /mnt/sdcard vfat rw,dirsync,nosuid,nodev,noexec,relatime,uid=1000,gid=1015,fmask=0702,dmask=0702,allow_utime=0020,codepage=cp437,iocharset=iso8859-1,shortname=mixed,utf8,errors=remount-ro 0 0\n" +
+//    "/dev/block/vold/179:1 /mnt/secure/asec vfat rw,dirsync,nosuid,nodev,noexec,relatime,uid=1000,gid=1015,fmask=0702,dmask=0702,allow_utime=0020,codepage=cp437,iocharset=iso8859-1,shortname=mixed,utf8,errors=remount-ro 0 0\n" +
+//    "tmpfs /mnt/sdcard/.android_secure tmpfs ro,relatime,size=0k,mode=000 0 0\n" +
+//    "/dev/block/dm-0 /mnt/asec/com.dasur.language.rus.pack-1 vfat ro,dirsync,nosuid,nodev,noexec,relatime,uid=1000,fmask=0222,dmask=0222,codepage=cp437,iocharset=iso8859-1,shortname=mixed,utf8,errors=remount-ro 0 0\n" +
+//    "/dev/block/dm-1 /mnt/asec/net.bytten.xkcdviewer-1 vfat ro,dirsync,nosuid,nodev,noexec,relatime,uid=1000,fmask=0222,dmask=0222,codepage=cp437,iocharset=iso8859-1,shortname=mixed,utf8,errors=remount-ro 0 0\n" +
+//    "/dev/block/dm-2 /mnt/asec/zok.android.shapes-2 vfat ro,dirsync,nosuid,nodev,noexec,relatime,uid=1000,fmask=0222,dmask=0222,codepage=cp437,iocharset=iso8859-1,shortname=mixed,utf8,errors=remount-ro 0 0\n" +
+//    "/dev/block/dm-3 /mnt/asec/net.hexage.everlands-1 vfat ro,dirsync,nosuid,nodev,noexec,relatime,uid=1000,fmask=0222,dmask=0222,codepage=cp437,iocharset=iso8859-1,shortname=mixed,utf8,errors=remount-ro 0 0\n" +
+//    "/dev/block/dm-3 /mnt/sdcard/maps ext2 ro,relatime,size=0k,mode=000 0 0\n";
+  
+  public static void reset() {
+    defaultStorage = null;
+    mountPoints = new TreeMap<String, MountPoint>();
+    init = false;
+  }
+}
---
+++ reference/web/diskusage/src/com/google/android/diskusage/entity/FileSystemPackage.java
@@ -0,0 +1,181 @@
+/**
+ * DiskUsage - displays sdcard usage on android.
+ * Copyright (C) 2008-2011 Ivan Volosyuk
+ *
+ * This program is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU General Public License
+ * as published by the Free Software Foundation; either version 2
+ * of the License, or (at your option) any later version.
+
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+
+ * You should have received a copy of the GNU General Public License
+ * along with this program; if not, write to the Free Software
+ * Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301, USA.
+ */
+
+package com.google.android.diskusage.entity;
+
+import android.content.pm.PackageStats;
+import android.util.Log;
+import com.google.android.diskusage.AppFilter;
+import com.google.android.diskusage.AppFilter.App2SD;
+
+import java.io.File;
+import java.util.ArrayList;
+import java.util.Arrays;
+
+public class FileSystemPackage extends FileSystemEntry {
+  public final String pkg;
+  final long codeSize;
+  final long dataSize;
+  final long cacheSize;
+  final long dalvikCacheSize;
+  final int flags;
+
+  // ApplicationInfo.FLAG_EXTERNAL_STORAGE
+  private static final int SDCARD_FLAG = 0x40000;
+
+
+  private File cacheFile(String pattern, Object... args) {
+    return new File("/data/dalvik-cache/" + String.format(pattern, args));
+  }
+
+  private File getDalvikCacheForSystemApp() {
+    File cache = cacheFile("system@app@%s.apk@classes.dex", name);
+    if (cache.exists()) return cache;
+    return null;
+  }
+
+  private File getDalvikCacheForNumberedPackage(String pattern) {
+    for (int i = 0; i < 10; i++) {
+      File cache = cacheFile(pattern, pkg, i);
+      if (cache.exists()) return cache;
+    }
+    return null;
+  }
+
+  private File getDalvikCacheForPkg(String pattern) {
+    File cache = cacheFile(pattern, pkg);
+    if (cache.exists()) return cache;
+    return null;
+  }
+
+  private long guessDalvikCacheSize() {
+    File cache = null;
+    if (onSD()) {
+      cache = getDalvikCacheForNumberedPackage(
+          "mnt@asec@%s-%d@pkg.apk@classes.dex");
+    } else {
+//      cache = getDalvikCacheForPkg("data@app@%s.apk@classes.dex");
+//      if (cache == null)
+//        cache = getDalvikCacheForPkg("data@app-private@%s.apk@classes.dex");
+//      if (cache == null)
+//        cache = getDalvikCacheForSystemApp();
+//      if (cache == null)
+//        cache = getDalvikCacheForNumberedPackage(
+//            "data@app@%s-%d.apk@classes.dex");
+//      if (cache == null)
+//        cache = getDalvikCacheForNumberedPackage(
+//        "data@app-private@%s-%d.apk@classes.dex");
+    }
+
+    if (cache != null) {
+      Log.d("diskusage", cache.getAbsolutePath() + ": " + cache.length());
+      return cache.length();
+    } else {
+//      Log.d("diskusage", "can't guess dalvikCache for " + pkg);
+      return 0;
+    }
+  }
+
+  public FileSystemPackage(
+      String name, String pkg, PackageStats stats,
+      int flags, Long hackApkSize, int blockSize) {
+    super(null, name);
+    this.pkg = pkg;
+    this.cacheSize = stats.cacheSize;
+    this.dataSize = stats.dataSize;
+    this.flags = flags | (hackApkSize != null ? SDCARD_FLAG : 0);
+    this.dalvikCacheSize = guessDalvikCacheSize();
+    if (onSD()) {
+      if (hackApkSize != null) {
+        this.codeSize = hackApkSize.intValue();
+      } else {
+        this.codeSize = stats.codeSize;
+      }
+    } else {
+      this.codeSize = stats.codeSize - this.dalvikCacheSize;
+    }
+  }
+
+  public boolean onSD() {
+    return (flags & SDCARD_FLAG) != 0;
+  }
+
+  private boolean select(App2SD selector, boolean isOnSD) {
+    return selector == App2SD.BOTH
+    || (isOnSD == (selector == App2SD.APPS2SD));
+  }
+
+  public void applyFilter(AppFilter filter, int blockSize) {
+    clearDrawingCache();
+    long blocks = 0;
+    ArrayList<FileSystemEntry> entries = new ArrayList<FileSystemEntry>();
+
+    if (select(filter.apps, onSD())) {
+      if (filter.showApk && select(filter.memory, onSD())) {
+        entries.add(FileSystemEntry.makeNode(null, "apk")
+            .initSizeInBytes(codeSize, blockSize));
+      }
+      if (filter.showData && select(filter.memory, false)) {
+        entries.add(FileSystemEntry.makeNode(null, "data")
+            .initSizeInBytes(dataSize, blockSize));
+      }
+      if (filter.showDalvikCache && select(filter.memory, false)) {
+        entries.add(FileSystemEntry.makeNode(null, "dalvikCache")
+            .initSizeInBytes(dalvikCacheSize, blockSize));
+      }
+      if (filter.showCache && select(filter.memory, false)) {
+      entries.add(FileSystemEntry.makeNode(null, "Cache")
+          .initSizeInBytes(cacheSize, blockSize));
+      }
+    }
+
+    for (FileSystemEntry e : entries) {
+      blocks += e.getSizeInBlocks();
+    }
+    setSizeInBlocks(blocks, blockSize);
+
+    if (filter.enableChildren) {
+      for (FileSystemEntry e : entries) {
+        e.parent = this;
+      }
+      children = entries.toArray(new FileSystemEntry[] {});
+      Arrays.sort(children, FileSystemEntry.COMPARE);
+    } else {
+      children = null;
+    }
+  }
+
+  private FileSystemPackage(
+      String name, String pkg, long codeSize, long dataSize, long cacheSize,
+      long dalvikCacheSize, int flags) {
+    super(null, name);
+    this.pkg = pkg;
+    this.codeSize = codeSize;
+    this.dataSize = dataSize;
+    this.cacheSize = cacheSize;
+    this.dalvikCacheSize = dalvikCacheSize;
+    this.flags = flags;
+  }
+
+  @Override
+  public FileSystemEntry create() {
+    return new FileSystemPackage(this.name, this.pkg, this.codeSize, this.dataSize, this.cacheSize,
+                                 this.dalvikCacheSize, this.flags);
+  }
+}
