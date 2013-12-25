bbfcd4e6927a85b37a09334a128340b0f8139b8b
ken <mailingonly@gmail.com>
2013-12-25 18:59:06 +0800

    added keyboard function, able to draw circle and rectangle and rotate

--- android-tutorial/opengl/c-example/chap01_doublebuf.c
+++ android-tutorial/opengl/c-example/chap01_doublebuf.c
@@ -2,12 +2,24 @@
 #include <GL/glu.h>
 #include <GL/glut.h>
 #include <stdlib.h>
+#include <math.h>
+
+/*
+ * compile
+g++ chap01_doublebuf.c -o ./bin/chap01_doublebuf -lglut -lGL -lGLU -lGLEW && ./bin/chap01_doublebuf
+ */
 
 /*
  * <hi> GLUT_DOUBLE </hi>
  */
 
+#define PI 3.1415926535898
 static GLfloat spin = 0.0;
+static int choice = 1;
+enum {
+	ROTATE_RECTANGLE=1,
+	DRAW_CIRCLE
+};
 
 void init(void)
 {
@@ -18,13 +30,39 @@ void init(void)
 
 void display(void)
 {
-	glClear(GL_COLOR_BUFFER_BIT);
-	glPushMatrix();
-	glRotatef(spin, 0.0, 0.0, 1.0);
-	glColor3f(1.0, 1.0, 1.0);
-	glRectf(-25.0, -25.0, 25.0, 25.0);
-	glPopMatrix();
-	glutSwapBuffers();
+	if ( choice == ROTATE_RECTANGLE ){
+		glClear(GL_COLOR_BUFFER_BIT);
+		glPushMatrix();
+		glRotatef(spin, 0.0, 0.0, 1.0);
+		glColor3f(1.0, 1.0, 1.0);
+		glRectf(-25.0, -25.0, 25.0, 25.0);
+		glPopMatrix();
+		glutSwapBuffers();
+	} else if ( choice == DRAW_CIRCLE){
+		glClear(GL_COLOR_BUFFER_BIT);
+		//glPushMatrix();
+		glRotatef(spin, 0.0, 0.0, 1.0);
+		glColor3f(1.0, 1.0, 0.0);
+		GLint circle_points = 100;
+		float factor = 5.0;
+		glBegin(GL_LINE_LOOP);
+			for (int i = 0; i < circle_points; i++) {
+				float angle = 2*PI*i/circle_points;
+				glVertex2f(cos(angle)*factor, sin(angle)*factor);
+			}
+		glEnd();
+		//glPopMatrix();
+
+		//glPushMatrix();
+		// draw a line crossing the circle
+		glBegin(GL_LINES);
+			glVertex2f(-10.0, -10.0);
+			glVertex2f(10.0, 10.0);
+		glEnd();
+		//glPopMatrix();
+
+		glutSwapBuffers();
+	}
 }
 
 void spinDisplay(void)
@@ -61,6 +99,26 @@ void mouse(int button, int state, int x, int y)
 	}
 }
 
+void keyboard(unsigned char key, int x, int y)
+{
+	switch(key) {
+		case 'c':
+		{
+			/// draw circle
+			choice = DRAW_CIRCLE;
+			break;
+		}
+		case 'r':
+		{
+			/// draw rectangle
+			choice = ROTATE_RECTANGLE;
+			break;
+		}
+		default: break;
+	}
+	glutPostRedisplay();
+}
+
 /*
  * request double buffer display mode
  */
@@ -77,6 +135,7 @@ int main(int argc, char **argv)
 	glutDisplayFunc(display);
 	glutReshapeFunc(reshape);
 	glutMouseFunc(mouse);
+	glutKeyboardFunc(keyboard);
 	glutMainLoop();
 	return 0;
 }
